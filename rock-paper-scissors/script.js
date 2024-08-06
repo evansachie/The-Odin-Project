61d5e4f7081deee57f37fdbedbfda1c775183cce
Evans Acheampong <evansachie01@gmail.com>
2024-08-06 10:22:21 +0000

    feat: add function to get human choice

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,13 +1,32 @@
+const prompt = require('prompt-sync')();
+
 function getComputerChoice(){
-    selection = Math.floor(Math.random() * 3)
+    let selection = Math.floor(Math.random() * 3)
 
     if (selection == 0){
-        return "You have chosen rock"
+        return "Comouter has chosen rock"
     } else if (selection == 1){
-        return "You have chosen Paper"
+        return "Computer has chosen Paper"
     } else {
-        return "You hae chosen scissors"
+        return "Computer has chosen scissors"
     }
 }
 
 // console.log(getComputerChoice())
+
+function getHumanChoice(){
+    let userInput = prompt("Enter a number(0 for Rock, 1 for Paper and 2 for Scissors): ")
+
+    if (parseInt(userInput) == 0){
+        return "You have chosen rock"
+    } else if (parseInt(userInput) == 1){
+        return "You have chosen Paper"
+    } else if (parseInt(userInput) == 2) {
+        return "You have chosen scissors"
+    } else {
+        return "Please enter a number from 0 - 2"
+    }
+
+}
+
+console.log(getHumanChoice())
