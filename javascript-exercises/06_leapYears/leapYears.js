d43a8545c327e5b517f6ef6c47994b99d3d03d52
Evans Acheampong <evansachie01@gmail.com>
2024-08-10 23:48:58 +0000

    feat: add javascript-exercise-6

---
+++ javascript-exercises/06_leapYears/leapYears.js
@@ -0,0 +1,10 @@
+const leapYears = function(leapYear) {
+    if (leapYear % 4 == 0 && (!(leapYear % 100 == 0) || leapYear % 400 == 0)){
+        return true
+    } else{
+        return false
+    }
+};
+
+// Do not edit below this line
+module.exports = leapYears;
