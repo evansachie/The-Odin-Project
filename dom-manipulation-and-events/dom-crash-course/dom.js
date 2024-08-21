7649b249d9d6a007ebecd61c306d6171f1447348
Evans Acheampong <evansachie01@gmail.com>
2024-08-21 13:40:44 +0000

    feat: add dom-crash-course-2

--- dom-manipulation-and-events/dom-crash-course/dom.js
+++ dom-manipulation-and-events/dom-crash-course/dom.js
@@ -51,7 +51,7 @@ for(var i = 0; i < li.length; i++ ){
 
 // QUERY SELECTOR
 var header = document.querySelector('main-header');
-header.style.borderBottom = 'solid 4px #ccc';
+// header.style.borderBottom = 'solid 4px #ccc';
 
 var input = document.querySelector('input');
 input.value = 'Hello World';
@@ -68,3 +68,52 @@ console.log(titles);
 titles[0].textContent = 'Hello';
 
 
+// JS DOM PART 2
+// TRAVERSING THE DOM
+var itemList = document.querySelector("#items");
+
+// parentNode - same as parentElement
+console.log(itemList.parentNode);
+itemList.parentNode.style.backgroundColor = "#f4f4f4";
+
+// childNodes / children
+console.log(itemList.childNodes);
+console.log(itemList.children);
+itemList.children[1].style.backgroundColor = 'pink';
+
+// firstElementChild
+console.log(itemList.firstElementChild);
+
+// lastElementChild
+console.log(itemList.lastElementChild);
+
+// siblings - null since there is no next element
+console.log(itemList.nextElementSibling);
+
+// prev sibling
+console.log(itemList.previousElementSibling);
+
+// createElement
+var newDiv = document.createElement('div');
+console.log(newDiv);
+
+// add id
+newDiv.id = 'div-id';
+
+// add className
+newDiv.className = 'hello';
+
+// add attr
+newDiv.setAttribute('title', 'Hello Div');
+
+var newDivText = document.createTextNode('Hello World');
+
+// add text to newDiv
+newDiv.appendChild(newDivText);
+
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
+
+container.insertBefore(newDiv, h1);
+ 
+newDiv.style.fontSize = '20px';
