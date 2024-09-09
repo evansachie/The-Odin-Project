1011cbd59024cd216a107e58a0721978da76798a
Evans Acheampong <evansachie01@gmail.com>
2024-09-09 06:38:47 +0000

    feat: add js-exercises:  fibonacci

---
+++ javascript-exercises/10_fibonacci/fibonacci.js
@@ -0,0 +1,18 @@
+const fibonacci = function(num) {
+    if (num <= 0){
+        return 0
+    }
+
+    if (num == 1 || num == 2){
+        return 1
+    }
+    
+    fib = [0, 1]
+    for (i = 2; i < num; i ++){
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    return fib[num - 1]
+};
+
+// Do not edit below this line
+module.exports = fibonacci;
