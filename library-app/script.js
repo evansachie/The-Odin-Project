2c8f4dbf4261cd64c80d4abdb78017011d841dea
Evans Acheampong <evansachie01@gmail.com>
2024-12-02 15:18:44 +0000

    update: refactor library script to use classses

--- library-app/script.js
+++ library-app/script.js
@@ -1,14 +1,16 @@
 const mylibrary = [];
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-}
+class Book{
+  constructor(title, author, pages, read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
 
-Book.prototype.toggleRead = function () {
-  this.read = !this.read;
+  toggleRead(){
+    this.read = !this.read;
+  }
 }
 
 function addBookToLibrary(title, author, pages, read) {
