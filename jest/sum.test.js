c13ab1efc75c4055710cca4064c10fdb4c89f928
Evans Acheampong <evansachie01@gmail.com>
2025-08-06 11:33:23 +0000

    feat: practice tdd with jest

---
+++ jest/sum.test.js
@@ -0,0 +1,63 @@
+const sum = require('./sum');
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+});
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).not.toBe(0);
+});
+
+test('object assignment', () => {
+  const data = {one: 1};
+  data['two'] = 2;
+  expect(data).toEqual({one: 1, two: 2});
+});
+
+test('sum of all pairs is correct', () => {
+    for (let a = 0; a < 5; a++){
+        for (let b = 0; b < 5; b++){
+            expect(sum(a, b)).toBe(a + b);
+        }
+    }
+});
+
+// Truthiness
+test('null check', () => {
+    const n = null;
+    expect(n).toBeNull();
+    expect(n).toBeDefined();
+    expect(n).toBeUndefined();
+})
+
+// Strings
+const shoppingList = [
+  'diapers',
+  'kleenex',
+  'trash bags',
+  'paper towels',
+  'milk',
+];
+
+test('the shopping list has milk on it', () => {
+  expect(shoppingList).toContain('milk');
+  expect(new Set(shoppingList)).toContain('milk');
+});
+
+// Exceptions
+function compileAndroidCode() {
+  throw new Error('you are using the wrong JDK!');
+}
+
+test('compiling android goes as expected', () => {
+  expect(() => compileAndroidCode()).toThrow();.
+  expect(() => compileAndroidCode()).toThrow(Error);
+
+  // You can also use a string that must be contained in the error message or a regexp
+  expect(() => compileAndroidCode()).toThrow('you are using the wrong JDK');
+  expect(() => compileAndroidCode()).toThrow(/JDK/);
+
+  // Or you can match an exact error message using a regexp like below
+  expect(() => compileAndroidCode()).toThrow(/^you are using the wrong JDK$/); // Test fails
+  expect(() => compileAndroidCode()).toThrow(/^you are using the wrong JDK!$/); // Test pass
+});
